1d07b76e4eb96e6e54db9b81a00fd58f3213ad3b
unknown <michaelmolina679@gmail.com>
2023-04-03 19:47:29 -0600

    new home view

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './Home.css';
+
+const Home = () => {
+    return (
+        <section className='Home'>
+            <div className="center">
+                Queremos saber tu opinión acerca de la
+                comunidad y sus necesidades. Ayúdanos
+                llenando esta pequeña encuesta, los
+                grandes camios llegan con un primer paso.
+                ¡Se parte de esto!
+            </div>
+        </section>
+    )
+}
+
+export default Home;
