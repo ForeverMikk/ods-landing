2f4fc99aa7943c0b8161d871011451213469a6ab
unknown <michaelmolina679@gmail.com>
2023-04-03 20:34:22 -0600

    adding images

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,17 +1,34 @@
 import React from 'react';
 
 import './Home.css';
+import logo  from '../../assets/logo-1.png';
+import logoOds  from '../../assets/logo-1.png';
+import footerImg from '../../assets/asset-14.png';
 
 const Home = () => {
     return (
-        <section className='Home'>
-            <div className="center">
-                Queremos saber tu opinión acerca de la
-                comunidad y sus necesidades. Ayúdanos
-                llenando esta pequeña encuesta, los
-                grandes camios llegan con un primer paso.
-                ¡Se parte de esto!
+        <section className='home'>
+
+            <nav>
+                <img src={logo} alt="Logo Gobierno de Hidalgo" />
+                <img src={logoOds} alt="Logo ODS" />
+            </nav>
+
+            <div className="main">
+                <p>
+                    Queremos saber tu opinión acerca de la
+                    comunidad y sus necesidades. Ayúdanos
+                    llenando esta pequeña encuesta, los
+                    grandes camios llegan con un primer paso.
+                    ¡Se parte de esto!
+                </p>
+
+                <button>Iniciar Encuaesta</button>
             </div>
+
+            <footer>
+                <div className="className"></div>
+            </footer>
         </section>
     )
 }
