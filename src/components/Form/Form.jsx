9c5c0d21dee2c9cc1773a86d5be0201c164efa1d
unknown <michaelmolina679@gmail.com>
2023-04-08 17:06:12 -0600

    adding styles and images in form container

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,43 +1,52 @@
 import React from 'react';
-import Popup from 'reactjs-popup';
+import { Link } from 'react-router-dom';
 
 import './Form.css';
+import icon from './../../assets/asset-2.png';
 import TermsConditions from '../TermsConditions/TermsConditions';
-import Navbar from '../Navbar/Navbar';
-import { Link } from 'react-router-dom';
 
 
 const Form = () => {
     return (
         <section className='form'>
-            <h1>Datos de usuario</h1>
+            <h1>
+                <span>Datos de usuario</span>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                </svg>
+            </h1>
 
-            <label htmlFor="name">Ingresa tu nombre completo</label>
-            <input name='name' type="text" placeholder='Nombre'/>
-
-            <label htmlFor="name">Slecciona tu municipio</label>
-            <input type="range" name="" id="" />
-
-            <div className="age-sex">
-                <div className="">
-                    <label htmlFor="age">Ingresa tu edad</label>
-                    <input type="text" name='' id=''/>
+            <div className="container">
+                <div className="question">
+                    <img src={icon} alt="" />
+                    <label htmlFor="name">Ingresa tu nombre completo</label>
+                    <input name='name' type="text" placeholder='Nombre'/>
                 </div>
 
-                <div className="">
-                    <label htmlFor="age">Selecciona tu sexo</label>
-                    <input type="text" name='' id=''/>
+                <div className="question">
+                    <img src={icon} alt="" />
+                    <label htmlFor="name">Slecciona tu municipio</label>
+                    <input type="range" name="" id="" />
+                </div>
+
+                <div className="age-sex">
+                    <div className="">
+                        <img src={icon} alt="" />
+                        <label htmlFor="age">Ingresa tu edad</label>
+                        <input type="text" name='' id=''/>
+                    </div>
+
+                    <div className="">
+                        <img src={icon} alt="" />
+                        <label htmlFor="age">Selecciona tu sexo</label>
+                        <input type="text" name='' id=''/>
+                    </div>
                 </div>
             </div>
             
             <Link to="/"> Regresar </Link>
-            <Popup 
-                trigger={<button type='button'> Enviar Cuestionarioa</button>} 
-                modal
-                className='terms-button'
-                >
             <TermsConditions />
-            </Popup>
+            
         </section>
     )
 }
--- src/components/TermsConditions/TermsConditions.jsx
+++ src/components/TermsConditions/TermsConditions.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
+import Popup from 'reactjs-popup';
 
+import './TermsConditions.css';
 
 const TermsConditions = () => {
     return(
-        <div className='terms-popup'>
-            <h1>Terminos y Condiciones</h1>
+        <Popup 
+            trigger={<button type='button'> Enviar Cuestionario</button>} 
+            modal
+            className='terms-button'
+        >
+            <div className='terms-popup'>
+                <h1>Terminos y Condiciones</h1>
 
-            <p>La utilización de este sitio y de cualquiera de las aplicaciones del mismo constituye el pleno y expreso consentimiento por parte del usuario para observar y sujetarse respecto de cada uno de los términos y condiciones que aquí se contienen, así como respecto de las políticas de privacidad, políticas de seguridad y, en su caso, cualesquiera otros documentos que conformen parte o regulen la participación del usuario en este sitio. En el supuesto que cualquiera de los términos a los que habrá de sujetarse el usuario cuando acceda a este sitio sean contrarios a sus intereses, deberá abstenerse de hacer uso de este.</p>
-            <br />
-            <p>La utilización de este sitio y de cualquiera de las aplicaciones del mismo constituye el pleno y expreso consentimiento por parte del usuario para observar y sujetarse respecto de cada uno de los términos y condiciones que aquí se contienen, así como respecto de las políticas de privacidad, políticas de seguridad y, en su caso, cualesquiera otros documentos que conformen parte o regulen la participación del usuario en este sitio. En el supuesto que cualquiera de los términos a los que habrá de sujetarse el usuario cuando acceda a este sitio sean contrarios a sus intereses, deberá abstenerse de hacer uso de este.</p>
+                <p>La utilización de este sitio y de cualquiera de las aplicaciones del mismo constituye el pleno y expreso consentimiento por parte del usuario para observar y sujetarse respecto de cada uno de los términos y condiciones que aquí se contienen, así como respecto de las políticas de privacidad, políticas de seguridad y, en su caso, cualesquiera otros documentos que conformen parte o regulen la participación del usuario en este sitio. En el supuesto que cualquiera de los términos a los que habrá de sujetarse el usuario cuando acceda a este sitio sean contrarios a sus intereses, deberá abstenerse de hacer uso de este.</p>
+                <br />
+                <p>La utilización de este sitio y de cualquiera de las aplicaciones del mismo constituye el pleno y expreso consentimiento por parte del usuario para observar y sujetarse respecto de cada uno de los términos y condiciones que aquí se contienen, así como respecto de las políticas de privacidad, políticas de seguridad y, en su caso, cualesquiera otros documentos que conformen parte o regulen la participación del usuario en este sitio. En el supuesto que cualquiera de los términos a los que habrá de sujetarse el usuario cuando acceda a este sitio sean contrarios a sus intereses, deberá abstenerse de hacer uso de este.</p>
 
-            <button type='button'> Más información </button>
-            
-            <div className='check'>
-                <input type="checkbox" name="accept" id="" />
-                <label htmlFor="accept">He leído y acepto los términos y condiciones</label>
+                <button type='button'> Más información </button>
+                
+                <div className='check'>
+                    <input type="checkbox" name="accept" id="" />
+                    <label htmlFor="accept">He leído y acepto los términos y condiciones</label>
+                </div>
+
             </div>
-
-        </div>
+        </Popup>
     )
 }
 
