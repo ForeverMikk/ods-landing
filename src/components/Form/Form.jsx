756c90d11ffdf037f8b8b114a7e183d4b1cb9c7a
unknown <michaelmolina679@gmail.com>
2023-04-08 15:57:56 -0600

    adding new inputs in form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,38 +2,38 @@ import React from 'react';
 import Popup from 'reactjs-popup';
 
 import './Form.css';
+import TermsConditions from '../TermsConditions/TermsConditions';
 
 
 const Form = () => {
     return (
         <div className='data-form'>
-            
-            <label htmlFor="name">Nombre</label>
+            <h1>Datos de usuario</h1>
+
+            <label htmlFor="name">Ingresa tu nombre completo</label>
             <input name='name' type="text" placeholder='Nombre'/>
 
-            <label htmlFor="name">Municipio</label>
+            <label htmlFor="name">Slecciona tu municipio</label>
             <input type="range" name="" id="" />
+
+            <div className="age-sex">
+                <div className="">
+                    <label htmlFor="age">Ingresa tu edad</label>
+                    <input type="text" name='' id=''/>
+                </div>
+
+                <div className="">
+                    <label htmlFor="age">Selecciona tu sexo</label>
+                    <input type="text" name='' id=''/>
+                </div>
+            </div>
             
             <Popup 
                 trigger={<button type='button'> Siguiente</button>} 
                 modal
                 className='terms-button'
                 >
-                <div className='terms-popup'>
-                    <h1>Terminos y Condiciones</h1>
-
-                    <p>La utilización de este sitio y de cualquiera de las aplicaciones del mismo constituye el pleno y expreso consentimiento por parte del usuario para observar y sujetarse respecto de cada uno de los términos y condiciones que aquí se contienen, así como respecto de las políticas de privacidad, políticas de seguridad y, en su caso, cualesquiera otros documentos que conformen parte o regulen la participación del usuario en este sitio. En el supuesto que cualquiera de los términos a los que habrá de sujetarse el usuario cuando acceda a este sitio sean contrarios a sus intereses, deberá abstenerse de hacer uso de este.</p>
-                    <br />
-                    <p>La utilización de este sitio y de cualquiera de las aplicaciones del mismo constituye el pleno y expreso consentimiento por parte del usuario para observar y sujetarse respecto de cada uno de los términos y condiciones que aquí se contienen, así como respecto de las políticas de privacidad, políticas de seguridad y, en su caso, cualesquiera otros documentos que conformen parte o regulen la participación del usuario en este sitio. En el supuesto que cualquiera de los términos a los que habrá de sujetarse el usuario cuando acceda a este sitio sean contrarios a sus intereses, deberá abstenerse de hacer uso de este.</p>
-
-                    <button type='button'> Más información </button>
-                    
-                    <div className='check'>
-                        <input type="checkbox" name="accept" id="" />
-                        <label htmlFor="accept">He leído y acepto los términos y condiciones</label>
-                    </div>
-
-                </div>
+               <TermsConditions />
             </Popup>
             
         </div>
--- src/components/TermsConditions/TermsConditions.jsx
+++ src/components/TermsConditions/TermsConditions.jsx
@@ -3,7 +3,21 @@ import React from 'react';
 
 const TermsConditions = () => {
     return(
-        <h1>Terminos a Condiciones</h1>
+        <div className='terms-popup'>
+            <h1>Terminos y Condiciones</h1>
+
+            <p>La utilización de este sitio y de cualquiera de las aplicaciones del mismo constituye el pleno y expreso consentimiento por parte del usuario para observar y sujetarse respecto de cada uno de los términos y condiciones que aquí se contienen, así como respecto de las políticas de privacidad, políticas de seguridad y, en su caso, cualesquiera otros documentos que conformen parte o regulen la participación del usuario en este sitio. En el supuesto que cualquiera de los términos a los que habrá de sujetarse el usuario cuando acceda a este sitio sean contrarios a sus intereses, deberá abstenerse de hacer uso de este.</p>
+            <br />
+            <p>La utilización de este sitio y de cualquiera de las aplicaciones del mismo constituye el pleno y expreso consentimiento por parte del usuario para observar y sujetarse respecto de cada uno de los términos y condiciones que aquí se contienen, así como respecto de las políticas de privacidad, políticas de seguridad y, en su caso, cualesquiera otros documentos que conformen parte o regulen la participación del usuario en este sitio. En el supuesto que cualquiera de los términos a los que habrá de sujetarse el usuario cuando acceda a este sitio sean contrarios a sus intereses, deberá abstenerse de hacer uso de este.</p>
+
+            <button type='button'> Más información </button>
+            
+            <div className='check'>
+                <input type="checkbox" name="accept" id="" />
+                <label htmlFor="accept">He leído y acepto los términos y condiciones</label>
+            </div>
+
+        </div>
     )
 }
 
